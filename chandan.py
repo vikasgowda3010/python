c9dc94fd1df8d44af53cba9618758aa71416236c
vikasgowda3010 <mgvikas5331@gmail.com>
2026-05-05 18:56:32 +0530

    printing

---
+++ chandan.py
@@ -0,0 +1,10 @@
+print("Namaskara!!!")
+
+print("Namaste, nanna hesaru Vikas!!!")
+
+print(11//3)
+
+boy_age= input("Enter the boy's age: ")
+girl_age= input("Enter the girl's age: ")
+
+print(f" The boy is {boy_age} years old and the girl is {girl_age} years old.")
